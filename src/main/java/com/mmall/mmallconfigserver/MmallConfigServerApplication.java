bd9cfbcb85fbd32f090e194dd070e0973037d95b
axuhan <axuhan@yeah.net>
2019-03-06 20:57:13 +0800

    first commit

---
+++ src/main/java/com/mmall/mmallconfigserver/MmallConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mmall.mmallconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmallConfigServerApplication.class, args);
+	}
+
+}
